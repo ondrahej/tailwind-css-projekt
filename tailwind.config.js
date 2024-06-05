7c0507e6163a4bf6ecb72d26cf996f68c1c317e3
Ondrej Hejduk <hejduk.on.2022@skola.ssps.cz>
2024-06-05 12:31:38 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    colors: {
+      primary: "#81A691",
+      secondary: "#C1BCCF",
+      box: "#81A691",
+
+
+    },
+  },
+  plugins: [],
+}
